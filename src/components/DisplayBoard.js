00288b848d78cfcbba72edba29b2cb24d74cb005
Jyrki Heinonen <jyrki.j.heinonen@cgi.com>
2022-09-28 12:37:39 +0300

    update

--- src/components/DisplayBoard.js
+++ src/components/DisplayBoard.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-export const DisplayBoard = ({numberOfUsers, getAllUsers}) => {
+export const DisplayBoard = ({ numberOfUsers, getAllUsers }) => {
 
-    const headerStyle = {
+    // const headerStyle = {
 
-        width: '100%',
-        padding: '2%',
-        backgroundColor: "red",
-        color: 'white',
-        textAlign: 'center'
-    }
-    
-    return(
-        <div style={{backgroundColor:'green'}} className="display-board">
-            <h4 style={{color: 'white'}}>Users Created</h4>
+    //     width: '100%',
+    //     padding: '2%',
+    //     backgroundColor: "red",
+    //     color: 'white',
+    //     textAlign: 'center'
+    // }
+
+    return (
+        <div style={{ backgroundColor: 'green' }} className="display-board">
+            <h4 style={{ color: 'white' }}>Users Created</h4>
             <div className="number">
-            {numberOfUsers}
+                {numberOfUsers}
             </div>
             <div className="btn">
                 <button type="button" onClick={(e) => getAllUsers()} className="btn btn-warning">Get all Users</button>
             </div>
         </div>
-    )
-}
+    );
+};
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,20 +1,20 @@
 
 export async function getAllUsers() {
 
-    try{
-        const response = await fetch('/api/users');
+    try {
+        const response = await fetch(`${process.env.REACT_API1_URL}/api/users`);
         return await response.json();
-    }catch(error) {
+    } catch (error) {
         return [];
     }
-    
+
 }
 
 export async function createUser(data) {
-    const response = await fetch(`/api/user`, {
+    const response = await fetch(`${process.env.REACT_API1_URL}/api/user`, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({user: data})
-      })
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ user: data })
+    });
     return await response.json();
 }
